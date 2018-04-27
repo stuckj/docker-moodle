fd3e7c6045156d150ab0e33150f923e7c771b435
John Stucklen <john.stucklen@runkeeper.com>
2018-04-27 00:52:40 -0400

    Forgot to enable the new apache configuration.

--- foreground.sh
+++ foreground.sh
@@ -13,6 +13,9 @@ trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 # Disable apache SSL if we're behind an SSL proxy
 if [ ${BEHIND_SSL_PROXY} -eq 1 ]; then
     a2dismod ssl && a2dissite default-ssl 
+    if [ ! -f /etc/apache2/conf-enabled/apache-proxy-servername.conf ]; then
+        ln -sf ../conf-available/apache-proxy-servername.conf /etc/apache2/conf-enabled/apache-proxy-servername.conf
+    fi
 fi
 
 source /etc/apache2/envvars
